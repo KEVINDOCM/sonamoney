1a04c3dd5ed8c6ddea35595d4a45cdbfc88ee9c7
KEVINDOCM <kevindocumentrgit@gmail.com>
2026-03-15 08:58:06 +0700

    SITEMAP

--- app/robots.ts
+++ app/robots.ts
@@ -19,6 +19,6 @@ export default function robots(): MetadataRoute.Robots {
         ],
       },
     ],
-    sitemap: "https://sonamoney.vercel.app/sitemap.xml",
+    sitemap: "https://sona-money.vercel.app/sitemap.xml",
   }
 }
